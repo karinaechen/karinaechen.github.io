d09a8917afd788770180103950c77f584aac09b2
Karina Chen <karina.e.chen@gmail.com>
2024-02-07 12:24:17 -0800

    update site

--- index.js
+++ index.js
@@ -1,36 +1,68 @@
-document.getElementById('main-landing').addEventListener("mousemove", evt => {
-  const mouseX = evt.clientX;
-  const mouseY = evt.clientY;
+// https://codepen.io/hi-im-si/pen/ALgzqo
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
-  gsap.set(".cursor", {
-    x: mouseX,
-    y: mouseY });
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-
-  gsap.to(".shape", {
-    x: mouseX,
-    y: mouseY,
-    stagger: -0.1 });
-
-});
-
-function Position(obj){
-  var currenttop = 0;
-  if (obj.offsetParent){
-    do {
-      currenttop += obj.offsetTop;
-    } while ((obj = obj.offsetParent));
-    return [currenttop];
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
- }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid rgba(255, 0, 0, 0)}";
+  document.body.appendChild(css);
+};
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  window.scrollTo({top: 0, behavior: 'smooth'});
-}
+// function topFunction() {
+//   window.scrollTo({top: 0, behavior: 'smooth'});
+// }
 
 // When the user clicks on the button, scroll to the next 
-function scrollNext(id_) {
-  let element = document.getElementById(id_);
-  window.scrollTo({top: Position(element), behavior: 'smooth'});
-}
+// function scrollNext(id_) {
+//   let element = document.getElementById(id_);
+//   window.scrollTo({top: Position(element), behavior: 'smooth'});
+// }
